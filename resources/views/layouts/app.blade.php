419542e92bbc9403668d7445a1312747cc50a564
Guilherme <guilhermedude.7@gmail.com>
2018-09-17 17:55:21 -0300

    Inicial

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    @if (Auth::guest())
+
+                    @else
+                        <li class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                               aria-haspopup="true" aria-expanded="false">
+                                {{ Auth::user()->name }}
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                                <a href="{{ route('logout') }}" class="dropdown-item"
+                                   onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                                    Logout
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                      style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </div>
+                        </li>
+                    @endif
+                </ul>
+            </div>
+
+        </div>
+    </nav>
+
+    @yield('content')
+</div>
+
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
---
+++ resources/views/venda/listagem.blade.php
@@ -0,0 +1,47 @@
+@extends('layout.principal')
+@section('conteudo')
+<div class="container">
+  <h2>Vendas</h2>     
+  <ul>
+      <li><a href="{{action('VendaController@novo')}}">Cadastrar Venda</a></li>
+  </ul>  
+  @if(session()->has('message.level'))
+    <div class="alert alert-{{ session('message.level') }}"> 
+      {!! session('message.content') !!}
+    </div>
+  @endif
+
+  <table id="listagemVendas" class="table table-bordered">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Nome Cliente</th>
+        <th>Valor Venda</th>
+        <th>Desconto R$</th>
+        <th>Desconto %</th>
+        <th>On-Line</th>
+        <th>Data</th>
+        <th>Horário</th>
+        <th>Editar Venda</th>
+        <th>Remover Venda</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach($vendas as $v)
+        <tr>
+          <td>{{ $v->id_venda }}</td>
+          <td>{{ $v->nome }}</td>
+          <td>R$ {{ $v->valor_venda }}</td>
+          <td>R$ {{ $v->desconto }}</td>
+          <td>{{ $v->porcentagem }}%</td>
+          <td>{{ $v->online == 1 ? 'Sim' : 'Não' }}</td>
+          <td>{{ date('d/m/Y', strtotime($v->created_at)) }}</td>
+          <td>{{ date('H:i:s', strtotime($v->created_at)) }}</td>
+          <td><a href="/ListarVenda/mostrar/{{ $v->id_venda }}"><span class="glyphicon glyphicon-pencil"></span></a></td>
+          <td><a href="/ListarVenda/remove/{{ $v->id_venda }}"><span class="glyphicon glyphicon-trash"></span></a></td>
+        </tr>
+      @endforeach
+    </tbody>
+  </table>
+</div>
+@stop
